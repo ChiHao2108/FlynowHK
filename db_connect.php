a38a3205ce7dcda60b7491da439932f06f1fe20f
ChiHao <chihao@example.com>
2026-01-19 16:33:55 +0700

    Đặt vé máy bay

--- db_connect.php
+++ db_connect.php
@@ -1,16 +1,14 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "48194007"; 
-$port = 3306; 
-$dbname = "flynow_db"; 
+$servername = getenv("DB_HOST");
+$username   = getenv("DB_USERNAME");
+$password   = getenv("DB_PASSWORD");
+$dbname     = getenv("DB_DATABASE");
+$port       = getenv("DB_PORT");
 
-$conn = new mysqli($servername, $username, $password, $dbname, $port); 
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 if ($conn->connect_error) {
     die("Kết nối database thất bại: " . $conn->connect_error);
 }
 
 $conn->set_charset("utf8mb4");
-
-?>
